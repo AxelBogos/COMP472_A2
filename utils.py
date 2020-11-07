cd44ab6773a63c092e6d5b0e366f8c3db68050f1
Xavier Morin <xavier10@live.ca>
2020-11-06 22:26:26 -0500

    code is scalable

--- utils.py
+++ utils.py
@@ -10,11 +10,13 @@ def generate_puzzles(nb,dim,path):
 def input_puzzles(path):
     puzzles=[]
     with open(path,'r') as f:
+        dim=f.readline()
+        dim=np.asarray(dim.split(),dtype='int32')
         for line in f:
             puzzle=line.split()
             puzzle=np.asarray(puzzle,dtype='int32')
             puzzles.append(puzzle)
-    return puzzles
+    return puzzles,dim[0],dim[1]
 
 def swap(state,id_1,id_2):
     new_state=state.copy()
